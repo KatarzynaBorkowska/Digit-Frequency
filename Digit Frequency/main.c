82e45b298bbf00a93c72c7138f85b69bc93a3205
KatarzynaBorkowska <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-06 22:10:11 +0100

    Update main.c

--- Digit Frequency/main.c
+++ Digit Frequency/main.c
@@ -1,14 +1,30 @@
-//
-//  main.c
-//  Digit Frequency
-//
-//  Created by Kasia on 06/03/2021.
-//
-
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    int n;
+    for(n = 0;s[n] != 0;n++)
+        ;
+    int tab[] = {0,0,0,0,0,0,0,0,0,0};
+    
+    for(int i = 0;i < 10;i++)
+    {
+        for(int j = 0;j < n;j++)
+        {
+            if(s[j] == (i%10)+48)
+                tab[i]++;
+        }
+    }
+    for(int i = 0;i < 10;i++)  
+        printf("%d ",tab[i]);
     return 0;
 }
