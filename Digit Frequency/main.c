b65cbe41f9b214d78cc6eb8e2d45ad14b89d87aa
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-06 22:00:58 +0100

    Initial Commit

---
+++ Digit Frequency/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Digit Frequency
+//
+//  Created by Kasia on 06/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
